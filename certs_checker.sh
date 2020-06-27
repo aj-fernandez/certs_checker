788d6a61b2e077f5a191116d10dcc692aeefd198
aj_fernandez <agustinjfd@outlook.es>
2020-06-27 17:40:08 +0200

    Comments removed

--- certs_checker.sh
+++ certs_checker.sh
@@ -9,19 +9,6 @@ channel="#productions_notifications"
 # Slack webhook
 webhook=""
 
-#ssl_check() {
-#    while IFS= read -r site;
-#    do
-#        #echo $site
-#        expire_epoch=$(openssl s_client -servername "$site" -connect "$site":"443" 2>&- | openssl x509 -enddate -noout | sed 's/^notAfter=//g' | xargs -I{} date -d {} +%s)
-#        if [ $epoch_warning -gt $expire_epoch ]; then
-#            alert="<$site> TLS certificate expiration date is 7 days or less"
-#            #payload="payload={\"channel\": \"$channel\", \"text\": \"${alert}\"}"
-#            payload="payload={\"tittle\": \"$tittle\",\"channel\": \"$channel\", \"text\": \"${alert}\"}"
-#            curl -s -m 10 --data-urlencode "${payload}" $webhook > /dev/null
-#        fi
-#    done <"./sites_list"
-#}
 
 expiration_harvesting() {
     while IFS= read -r site;
